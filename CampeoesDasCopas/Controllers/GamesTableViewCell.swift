790c4f939d013420f36108fedebd7f70f19b92c9
GabrielCarvalhoM <c.gabrielmaciel@icloud.com>
2022-10-04 21:37:00 -0300

    subindo app

---
+++ CampeoesDasCopas/Controllers/GamesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GamesTableViewCell.swift
+//  CampeoesDasCopas
+//
+//  Created by Gabriel Carvalho on 04/10/22.
+//
+
+import UIKit
+
+class GamesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CampeoesDasCopas/Models/WorldCup.swift
@@ -0,0 +1,8 @@
+//
+//  WorldCup.swift
+//  CampeoesDasCopas
+//
+//  Created by Gabriel Carvalho on 29/09/22.
+//
+
+import Foundation
